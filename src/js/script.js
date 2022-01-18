db36abfa9267747f935e61141d447ad146e09ef6
Alex <liderfrakzii@gmail.com>
2022-01-18 22:01:32 +0200

    Carousel is done

--- src/js/script.js
+++ src/js/script.js
@@ -2,6 +2,12 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         infinite: false,
         centerMode: true,
+        slidesToShow: 3,
+        dots: false,
+        variableWidth: true,
+        speed: 1000,
+        initialSlide: 1,
+        adaptiveHeight: true,
         prevArrow:
             `<button type="button" class="slick-prev">
                 <img src="icons/left_arrow.png" alt="prev">
