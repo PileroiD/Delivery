505bf357829183857ceb880a3492111039329c30
Alex <liderfrakzii@gmail.com>
2022-01-21 22:14:50 +0200

    Modal screen is done

--- src/js/script.js
+++ src/js/script.js
@@ -16,23 +16,38 @@ $(document).ready(function(){
             `<button type="button" class="slick-next">
             <img src="icons/right_arrow.png" alt="prev">
             </button>`,
-        responsive: [
-            {
-                breakpoint: 992,
-                settings: {
-                    dots: true,
-                    arrows: false,
-                }
-
-            },
-            {
-                breakpoint: 768,
-                settings: {
-                    dots: true,
-                    arrows: false,
-                }
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                dots: false,
+                arrows: false,
+                infinite: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
             }
-        ]
+        }],
     });
+
+    const overlay = document.querySelector('.overlay');
+    const btn = document.querySelector('.button.button_promo_2');
+    const closeBtn = document.querySelector('.modal__close');
+
+    btn.addEventListener('click', openModal);
+    overlay.addEventListener('click', closeModal);
+    closeBtn.addEventListener('click', closeModal);
+
+    document.addEventListener('keydown', (event) => {
+        if (event.code == 'Escape' && overlay.style.display == 'block') {
+            closeModal();
+        }
+    });
+
+    function closeModal() {
+        $('.overlay, #consultation, #thanks').fadeOut();
+    }
+
+    function openModal() {
+        $('.overlay, #consultation').fadeIn();
+    }
 
 });
